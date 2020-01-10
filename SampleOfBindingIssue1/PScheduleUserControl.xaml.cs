1a8ccfc01ba2babe363ce26937f6f6d1c2cfa5fe
Jignesh Desai <jignesh_desai@freytag.us>
2020-01-10 14:21:52 +0530

    Changes as per suggestion of Martin Zikmund   (  ItemsSource="{Binding ElementName=UCpage, Path=dpl}"  )

---
+++ SampleOfBindingIssue1/PScheduleUserControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SampleOfBindingIssue1
+{
+    public sealed partial class PScheduleUserControl : UserControl
+    {
+        public ObservableCollection<DynamicPricingList> dpl = new ObservableCollection<DynamicPricingList>();
+        
+        public PScheduleUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            dpl.Add(new DynamicPricingList("Free"));
+            for (int i = 1; i < 24; i++)
+            {
+                dpl.Add(new DynamicPricingList(string.Format("{0} USD", i)));
+            }
+
+            int xx = 0;
+           
+        }
+    }
+
+
+    public class DynamicPricingList
+    {
+        public string PriceValue { get; set; }
+        public DynamicPricingList() { }
+        public DynamicPricingList(string pPriceValue)
+        {
+            PriceValue = pPriceValue;
+        }
+    }
+}
